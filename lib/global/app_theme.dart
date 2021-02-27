a99eb18585b6aeede63ed93d5aeaadaba0582372
Ethan Hopkins <58455740+GlowingEmber@users.noreply.github.com>
2021-02-27 14:54:16 -0800

    ListView images/labeled

--- lib/global/app_theme.dart
+++ lib/global/app_theme.dart
@@ -6,4 +6,5 @@ class AppTheme {
   static const Color primaryText = Color(0xFF000000);
   static const Color secondaryText = Color(0xFF000000);
   static const Color buttonText = Color(0xFFFFFFFF);
+  static const Color backgroundLightGray = Color(0xFFe3e3e3);
 }
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,8 +1,17 @@
+import 'package:app/components/themed_text.dart';
+import 'package:app/global/app_theme.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  final List<String> list = ['hey', 'hi', 'ho', 'hum', 'phi', 'fi'];
+  final List<String> list = [
+    'Flutter Development',
+    'Logo Design',
+    'ho',
+    'hum',
+    'phi',
+    'fi'
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -10,7 +19,36 @@ class HomePage extends StatelessWidget {
         body: ListView.builder(
             itemCount: list.length,
             itemBuilder: (c, i) {
-              return Text(list[i]);
+              return Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Container(
+                    color: AppTheme.backgroundLightGray,
+                    child: Column(
+                      children: [
+                        Image.asset('assets/nooblow.png',
+                            height: 250, width: 400, fit: BoxFit.cover),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 10, horizontal: 15),
+                          child: SizedBox(
+                            width: double.infinity,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                ThemedText(list[i], type: Type.h2),
+                                ThemedText('Ben Swerdlow', type: Type.subtitle)
+                              ],
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              );
+              //Text(list[i]);
             }));
   }
 }
