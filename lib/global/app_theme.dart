fdacbaeda5fe5fa2ffd50149ccdadf6f4cfd86b3
jasonTelanoff <jason.telanoff@gmail.com>
2021-02-26 17:53:14 -0800

    added structure

---
+++ lib/global/app_theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static const Color primary = Color(0xFF000000);
+  static const Color secondary = Color(0xFF000000);
+  static const Color primaryText = Color(0xFF000000);
+  static const Color secondaryText = Color(0xFF000000);
+  static const Color buttonText = Color(0xFF000000);
+}
