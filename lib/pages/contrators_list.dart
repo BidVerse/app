1259ad4fe66df9b90f991d552a74e5ac8f3efce2
jasonTelanoff <jason.telanoff@gmail.com>
2021-02-27 17:05:12 -0800

    moved page to a custom scroll view

---
+++ lib/pages/contrators_list.dart
@@ -0,0 +1,52 @@
+import 'package:app/components/header.dart';
+import 'package:app/components/themed_text.dart';
+import 'package:app/global/app_theme.dart';
+import 'package:flutter/material.dart';
+
+class ContractorListPage extends StatelessWidget {
+  final List<String> list = [
+    'Flutter Development',
+    'Logo Design',
+    'Parametric Logo Design',
+    'React Development',
+    'Project Management'
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(slivers: [
+      Header('List'),
+      SliverList(
+          delegate:
+              SliverChildBuilderDelegate((c, i) => Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Container(
+                color: AppTheme.backgroundLightGray,
+                child: Column(
+                  children: [
+                    Image.asset('assets/nooblow.png',
+                        height: 250, width: 400, fit: BoxFit.cover),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 10, horizontal: 15),
+                      child: SizedBox(
+                        width: double.infinity,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            ThemedText(list[i], type: Type.h2),
+                            ThemedText('Ben Swerdlow', type: Type.subtitle)
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          );, childCount: list.length))
+    ]);
+  }
+}
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,53 +1,11 @@
-import 'package:app/components/themed_text.dart';
-import 'package:app/global/app_theme.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
-  final List<String> list = [
-    'Flutter Development',
-    'Logo Design',
-    'Parametric Logo Design',
-    'React Development',
-    'Project Management'
-  ];
+import 'contrators_list.dart';
 
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: ListView.builder(
-            itemCount: list.length,
-            itemBuilder: (c, i) {
-              return Padding(
-                padding: const EdgeInsets.all(12.0),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(20),
-                  child: Container(
-                    color: AppTheme.backgroundLightGray,
-                    child: Column(
-                      children: [
-                        Image.asset('assets/nooblow.png',
-                            height: 250, width: 400, fit: BoxFit.cover),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(
-                              vertical: 10, horizontal: 15),
-                          child: SizedBox(
-                            width: double.infinity,
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                ThemedText(list[i], type: Type.h2),
-                                ThemedText('Ben Swerdlow', type: Type.subtitle)
-                              ],
-                            ),
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                ),
-              );
-              //Text(list[i]);
-            }));
+    return Scaffold(body: ContractorListPage());
   }
 }
