082a211d3a83815124c6560736abdc816c4ac730
jasonTelanoff <jason.telanoff@gmail.com>
2021-02-28 11:01:32 -0800

    now

--- lib/pages/contrators_list.dart
+++ lib/pages/contrators_list.dart
@@ -2,6 +2,7 @@ import 'package:app/components/header.dart';
 import 'package:app/components/profile_tile.dart';
 import 'package:app/components/themed_text.dart';
 import 'package:app/global/app_theme.dart';
+import 'package:app/models/bid.dart';
 import 'package:app/models/profiles.dart';
 import 'package:app/pages/contractor_info.dart';
 import 'package:flutter/cupertino.dart';
@@ -10,45 +11,53 @@ import 'package:flutter/material.dart';
 class ContractorListPage extends StatelessWidget {
   final List<Profile> list = [
     new Profile(
-      contract: 'Mobile App Development',
-      name: 'Jacob Zwang',
-      image: 'assets/nooblow.png',
-      id: '1',
-      description:
-          'Nulla do cupidatat non duis officia sit esse sit qui duis voluptate cupidatat ex magna. Proident aute proident minim quis ipsum duis ex nisi. Ea proident cupidatat duis consequat ullamco cupidatat in dolor quis id veniam reprehenderit duis. Proident elit culpa dolor enim esse et do non irure ad nisi velit excepteur. Non culpa mollit consectetur aliquip duis nostrud qui est magna id.',
-      bio:
-          'Tempor sint proident mollit enim incididunt ex. Aliquip duis in ullamco eu consequat. Officia deserunt incididunt sit esse labore est id ex. Eu excepteur excepteur non duis veniam et. In minim dolor enim in ad reprehenderit ad irure sit eu sint id ex adipisicing. ',
-    ),
+        contract: 'Mobile App Development',
+        name: 'Jacob Zwang',
+        image: 'assets/nooblow.png',
+        id: '1',
+        description:
+            'Nulla do cupidatat non duis officia sit esse sit qui duis voluptate cupidatat ex magna. Proident aute proident minim quis ipsum duis ex nisi. Ea proident cupidatat duis consequat ullamco cupidatat in dolor quis id veniam reprehenderit duis. Proident elit culpa dolor enim esse et do non irure ad nisi velit excepteur. Non culpa mollit consectetur aliquip duis nostrud qui est magna id.',
+        bio:
+            'Tempor sint proident mollit enim incididunt ex. Aliquip duis in ullamco eu consequat. Officia deserunt incididunt sit esse labore est id ex. Eu excepteur excepteur non duis veniam et. In minim dolor enim in ad reprehenderit ad irure sit eu sint id ex adipisicing. ',
+        bids: [
+          new Bid(amount: 100, name: 'Benjamin Swerdlow'),
+        ]),
     new Profile(
-      contract: 'Modern Logo Design - Illustrator',
-      name: 'Ben Swerdlow',
-      image: 'assets/logo.png',
-      id: '2',
-      description:
-          'Nulla do cupidatat non duis officia sit esse sit qui duis voluptate cupidatat ex magna. Proident aute proident minim quis ipsum duis ex nisi. Ea proident cupidatat duis consequat ullamco cupidatat in dolor quis id veniam reprehenderit duis. Proident elit culpa dolor enim esse et do non irure ad nisi velit excepteur. Non culpa mollit consectetur aliquip duis nostrud qui est magna id.',
-      bio:
-          'Tempor sint proident mollit enim incididunt ex. Aliquip duis in ullamco eu consequat. Officia deserunt incididunt sit esse labore est id ex. Eu excepteur excepteur non duis veniam et. In minim dolor enim in ad reprehenderit ad irure sit eu sint id ex adipisicing. ',
-    ),
+        contract: 'Modern Logo Design - Illustrator',
+        name: 'Ben Swerdlow',
+        image: 'assets/logo.png',
+        id: '2',
+        description:
+            'Nulla do cupidatat non duis officia sit esse sit qui duis voluptate cupidatat ex magna. Proident aute proident minim quis ipsum duis ex nisi. Ea proident cupidatat duis consequat ullamco cupidatat in dolor quis id veniam reprehenderit duis. Proident elit culpa dolor enim esse et do non irure ad nisi velit excepteur. Non culpa mollit consectetur aliquip duis nostrud qui est magna id.',
+        bio:
+            'Tempor sint proident mollit enim incididunt ex. Aliquip duis in ullamco eu consequat. Officia deserunt incididunt sit esse labore est id ex. Eu excepteur excepteur non duis veniam et. In minim dolor enim in ad reprehenderit ad irure sit eu sint id ex adipisicing. ',
+        bids: [
+          new Bid(amount: 100, name: 'Benjamin Swerdlow'),
+        ]),
     new Profile(
-      contract: 'Project/Pipeline Management',
-      name: 'Jason Telanoff',
-      image: 'assets/logo.png',
-      id: '3',
-      description:
-          'Nulla do cupidatat non duis officia sit esse sit qui duis voluptate cupidatat ex magna. Proident aute proident minim quis ipsum duis ex nisi. Ea proident cupidatat duis consequat ullamco cupidatat in dolor quis id veniam reprehenderit duis. Proident elit culpa dolor enim esse et do non irure ad nisi velit excepteur. Non culpa mollit consectetur aliquip duis nostrud qui est magna id.',
-      bio:
-          'Tempor sint proident mollit enim incididunt ex. Aliquip duis in ullamco eu consequat. Officia deserunt incididunt sit esse labore est id ex. Eu excepteur excepteur non duis veniam et. In minim dolor enim in ad reprehenderit ad irure sit eu sint id ex adipisicing. ',
-    ),
+        contract: 'Project/Pipeline Management',
+        name: 'Jason Telanoff',
+        image: 'assets/logo.png',
+        id: '3',
+        description:
+            'Nulla do cupidatat non duis officia sit esse sit qui duis voluptate cupidatat ex magna. Proident aute proident minim quis ipsum duis ex nisi. Ea proident cupidatat duis consequat ullamco cupidatat in dolor quis id veniam reprehenderit duis. Proident elit culpa dolor enim esse et do non irure ad nisi velit excepteur. Non culpa mollit consectetur aliquip duis nostrud qui est magna id.',
+        bio:
+            'Tempor sint proident mollit enim incididunt ex. Aliquip duis in ullamco eu consequat. Officia deserunt incididunt sit esse labore est id ex. Eu excepteur excepteur non duis veniam et. In minim dolor enim in ad reprehenderit ad irure sit eu sint id ex adipisicing. ',
+        bids: [
+          new Bid(amount: 100, name: 'Benjamin Swerdlow'),
+        ]),
     new Profile(
-      contract: 'Perl Development',
-      name: 'Ethan Hopkins',
-      image: 'assets/logo.png',
-      id: '4',
-      description:
-          'Nulla do cupidatat non duis officia sit esse sit qui duis voluptate cupidatat ex magna. Proident aute proident minim quis ipsum duis ex nisi. Ea proident cupidatat duis consequat ullamco cupidatat in dolor quis id veniam reprehenderit duis. Proident elit culpa dolor enim esse et do non irure ad nisi velit excepteur. Non culpa mollit consectetur aliquip duis nostrud qui est magna id.',
-      bio:
-          'Tempor sint proident mollit enim incididunt ex. Aliquip duis in ullamco eu consequat. Officia deserunt incididunt sit esse labore est id ex. Eu excepteur excepteur non duis veniam et. In minim dolor enim in ad reprehenderit ad irure sit eu sint id ex adipisicing. ',
-    ),
+        contract: 'Perl Development',
+        name: 'Ethan Hopkins',
+        image: 'assets/logo.png',
+        id: '4',
+        description:
+            'Nulla do cupidatat non duis officia sit esse sit qui duis voluptate cupidatat ex magna. Proident aute proident minim quis ipsum duis ex nisi. Ea proident cupidatat duis consequat ullamco cupidatat in dolor quis id veniam reprehenderit duis. Proident elit culpa dolor enim esse et do non irure ad nisi velit excepteur. Non culpa mollit consectetur aliquip duis nostrud qui est magna id.',
+        bio:
+            'Tempor sint proident mollit enim incididunt ex. Aliquip duis in ullamco eu consequat. Officia deserunt incididunt sit esse labore est id ex. Eu excepteur excepteur non duis veniam et. In minim dolor enim in ad reprehenderit ad irure sit eu sint id ex adipisicing. ',
+        bids: [
+          new Bid(amount: 100, name: 'Benjamin Swerdlow'),
+        ]),
   ];
 
   @override
