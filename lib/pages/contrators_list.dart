e6c8cbd2566442beba563a7d182381994dc67c5a
Ethan Hopkins <58455740+GlowingEmber@users.noreply.github.com>
2021-02-27 17:24:08 -0800

    added search bar

--- lib/pages/contrators_list.dart
+++ lib/pages/contrators_list.dart
@@ -1,6 +1,7 @@
 import 'package:app/components/header.dart';
 import 'package:app/components/themed_text.dart';
 import 'package:app/global/app_theme.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class ContractorListPage extends StatelessWidget {
@@ -16,38 +17,47 @@ class ContractorListPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: CustomScrollView(slivers: [
-        Header('List'),
+        CupertinoSliverNavigationBar(
+            automaticallyImplyLeading: false,
+            largeTitle: Padding(
+              padding: const EdgeInsets.only(right: 12),
+              child: CupertinoSearchTextField(),
+            ),
+            middle: ThemedText('List', type: Type.h1)),
         SliverList(
-            delegate:
-                SliverChildBuilderDelegate((c, i) => Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(20),
-                child: Container(
-                  color: AppTheme.backgroundLightGray,
-                  child: Column(
-                    children: [
-                      Image.asset('assets/nooblow.png',
-                          height: 250, width: 400, fit: BoxFit.cover),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 10, horizontal: 15),
-                        child: SizedBox(
-                          width: double.infinity,
+            delegate: SliverChildBuilderDelegate(
+                (c, i) => Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(20),
+                        child: Container(
+                          color: AppTheme.backgroundLightGray,
                           child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
-                              ThemedText(list[i], type: Type.h2),
-                              ThemedText('Ben Swerdlow', type: Type.subtitle)
+                              Image.asset('assets/nooblow.png',
+                                  height: 250, width: 400, fit: BoxFit.cover),
+                              Padding(
+                                padding: const EdgeInsets.symmetric(
+                                    vertical: 10, horizontal: 15),
+                                child: SizedBox(
+                                  width: double.infinity,
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      ThemedText(list[i], type: Type.h2),
+                                      ThemedText('Ben Swerdlow',
+                                          type: Type.subtitle)
+                                    ],
+                                  ),
+                                ),
+                              )
                             ],
                           ),
                         ),
-                      )
-                    ],
-                  ),
-                ),
-              ),
-            );, childCount: list.length))
+                      ),
+                    ),
+                childCount: list.length))
       ]),
     );
   }
